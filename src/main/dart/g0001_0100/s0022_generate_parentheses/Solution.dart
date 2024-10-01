a801b64799d08aeb63d1cbeb71cbbce8a33f7a9c
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-01 18:44:54 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0022_generate_parentheses/Solution.dart
+++ src/main/dart/g0001_0100/s0022_generate_parentheses/Solution.dart
@@ -7,20 +7,20 @@ class Solution {
       return parenthesis(n , n);
   }
   List<String> parenthesis(int left , int right){
-      if(left == 0 && right == 0){
+      if (left == 0 && right == 0) {
           return [""];
       }
       List<String> return_ls = [];
-      if(left != 0){
+      if (left != 0) {
           List<String> l1 = parenthesis(left - 1 , right);
-          for(int i = 0 ; i < l1.length ; ++i){
+          for (int i = 0 ; i < l1.length ; ++i) {
             l1[i] = '(' + l1[i];
             return_ls.add(l1[i]);
           }
       }
-      if(right != 0 && left < right){
+      if (right != 0 && left < right) {
           List<String> r1 = parenthesis(left , right - 1);
-          for(int i = 0 ; i < r1.length ; ++i){
+          for (int i = 0 ; i < r1.length ; ++i) {
               r1[i] = ')' + r1[i];
               return_ls.add(r1[i]);
           }
