218de0d23de0cd814bb7d9a8aaee77805ffa0b42
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-04 18:02:12 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0041_first_missing_positive/Solution.dart
+++ src/main/dart/g0001_0100/s0041_first_missing_positive/Solution.dart
@@ -2,28 +2,27 @@
 // #Big_O_Time_O(n)_Space_O(n) #2024_10_04_Time_355_ms_(85.19%)_Space_167.8_MB_(51.85%)
 
 class Solution {
-    int firstMissingPositive(List<int> nums) {
+  int firstMissingPositive(List<int> nums) {
+    int i = 0, n = nums.length;
 
-        int i = 0, n = nums.length;
-
-        while (i < n) {
-            int j = nums[i] - 1;
-            if (0 <= j && j < n && nums[i] != nums[j]) {
-                // swap
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-            } else {
-                i ++;
-            }
-        }
-
-        for (int i = 0; i < n; i ++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
-        }
-
-        return n + 1;
+    while (i < n) {
+      int j = nums[i] - 1;
+      if (0 <= j && j < n && nums[i] != nums[j]) {
+        // swap
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+      } else {
+        i++;
+      }
     }
+
+    for (int i = 0; i < n; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+
+    return n + 1;
+  }
 }
