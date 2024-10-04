19fef1211688357fafbd3195fe39c99cbc7c84c5
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-04 18:07:25 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0051_n_queens/Solution.dart
+++ src/main/dart/g0001_0100/s0051_n_queens/Solution.dart
@@ -2,57 +2,57 @@
 // #2024_10_04_Time_328_ms_(86.67%)_Space_149.1_MB_(73.33%)
 
 class Solution {
-    List<List<String>> solveNQueens(int n) {
-        final List<List<String>> board = List.generate(n, (_) => List<String>.filled(n, '.'));
-        final List<List<String>> ans = [];
-        backtrack(board, ans, 0, 0, n);
-        return ans;
+  List<List<String>> solveNQueens(int n) {
+    final List<List<String>> board = List.generate(n, (_) => List<String>.filled(n, '.'));
+    final List<List<String>> ans = [];
+    backtrack(board, ans, 0, 0, n);
+    return ans;
+  }
+
+  bool isSafe(List<List<String>> board, int row, int col) {
+    for (int i = 0; i < row; i++) {
+      if (board[i][col] == 'Q') {
+        return false;
+      }
     }
 
-    bool isSafe(List<List<String>> board, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
-        }
-
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        return true;
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] == 'Q') {
+        return false;
+      }
     }
 
-    void backtrack(List<List<String>> board, List<List<String>> ans, int row, int col, int queens) {
-        if (row == queens) {
-            ans.add(convert(board));
-            return;
-        }
-
-        for (int col = 0; col < board.length; col++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                backtrack(board, ans, row + 1, col, queens);
-                board[row][col] = '.';
-            }
-        }
+    for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+      if (board[i][j] == 'Q') {
+        return false;
+      }
     }
 
-    List<String> convert(List<List<String>> board) {
-        final List<String> ans = [];
-        for (int i = 0; i < board.length; i++) {
-            final row = board[i].join('');
-            ans.add(row);
-        }
+    return true;
+  }
 
-        return ans;
+  void backtrack(List<List<String>> board, List<List<String>> ans, int row, int col, int queens) {
+    if (row == queens) {
+      ans.add(convert(board));
+      return;
     }
+
+    for (int col = 0; col < board.length; col++) {
+      if (isSafe(board, row, col)) {
+        board[row][col] = 'Q';
+        backtrack(board, ans, row + 1, col, queens);
+        board[row][col] = '.';
+      }
+    }
+  }
+
+  List<String> convert(List<List<String>> board) {
+    final List<String> ans = [];
+    for (int i = 0; i < board.length; i++) {
+      final row = board[i].join('');
+      ans.add(row);
+    }
+
+    return ans;
+  }
 }
