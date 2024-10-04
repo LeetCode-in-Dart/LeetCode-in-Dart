ffe7d51acf87bbd35936674f82ca822151643e07
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-04 18:02:57 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0042_trapping_rain_water/Solution.dart
+++ src/main/dart/g0001_0100/s0042_trapping_rain_water/Solution.dart
@@ -3,19 +3,21 @@
 // #Big_O_Time_O(n)_Space_O(1) #2024_10_04_Time_328_ms_(80.77%)_Space_153.5_MB_(17.31%)
 
 class Solution {
-    int trap(List<int> heigth) {
-        int left = 0,
-                right = heigth.length - 1,
-                leftMax = 0,
-                rightMax = 0,
-                water = 0;
-        while (left < right) {
-            leftMax = max(leftMax, heigth[left]);
-            rightMax = max(rightMax, heigth[right]);
-            water += leftMax < rightMax
-                    ? leftMax - heigth[left++]
-                    : rightMax - heigth[right--];
-        }
-        return water;
+  int trap(List<int> heigth) {
+    int left = 0,
+        right = heigth.length - 1,
+        leftMax = 0,
+        rightMax = 0,
+        water = 0;
+        
+    while (left < right) {
+      leftMax = max(leftMax, heigth[left]);
+      rightMax = max(rightMax, heigth[right]);
+      water += leftMax < rightMax
+          ? leftMax - heigth[left++]
+          : rightMax - heigth[right--];
     }
+    
+    return water;
+  }
 }
