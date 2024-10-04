0caab8837243ed46de9f9beed2171d3d50514268
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-04 18:04:35 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0046_permutations/Solution.dart
+++ src/main/dart/g0001_0100/s0046_permutations/Solution.dart
@@ -4,25 +4,26 @@
 // #2024_10_04_Time_319_ms_(80.00%)_Space_146.8_MB_(92.00%)
 
 class Solution {
-    List<List<int>> permute(List<int> nums) {
-        List<List<int>> ans = [];
-        List<bool> used = List<bool>.filled(nums.length, false);
-        helper(ans , [], used, nums);
-        return ans;
+  List<List<int>> permute(List<int> nums) {
+    List<List<int>> ans = [];
+    List<bool> used = List<bool>.filled(nums.length, false);
+    helper(ans, [], used, nums);
+    return ans;
+  }
+  
+  void helper(List<List<int>> ans, List<int> temp, List<bool> used, List<int> nums) {
+    if (temp.length == nums.length) {
+      ans.add(List<int>.from(temp));
+      return;
     }
-    void helper (List<List<int>> ans,List<int> temp, List<bool> used,List<int> nums ){
-        if(temp.length == nums.length){
-            ans.add(List<int>.from(temp));
-            return;
-        }
-        for(int i=0; i<nums.length; i++){
-            if(used[i]) continue;
-            temp.add(nums[i]);
-            used[i] = true;
-            helper(ans, temp, used, nums);
-            temp.removeLast();
-            used[i] = false;
-
-        }
+    
+    for (int i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+      temp.add(nums[i]);
+      used[i] = true;
+      helper(ans, temp, used, nums);
+      temp.removeLast();
+      used[i] = false;
     }
+  }
 }
