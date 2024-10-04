cf7a77e7d4ff7c85be590487b44b6ddbd9fba22c
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-04 18:08:14 +0300

    Update Solution.dart

--- src/main/dart/g0001_0100/s0053_maximum_subarray/Solution.dart
+++ src/main/dart/g0001_0100/s0053_maximum_subarray/Solution.dart
@@ -4,18 +4,21 @@
 // #2024_10_04_Time_361_ms_(88.89%)_Space_182.4_MB_(7.41%)
 
 class Solution {
-    int maxSubArray(List<int> nums) {
-        int maxi = -1 << 31; // Equivalent to Integer.MIN_VALUE
-        int sum = 0;
-        for (int num in nums) {
-            // calculating sub-array sum
-            sum += num;
-            maxi = sum > maxi ? sum : maxi;
-            if (sum < 0) {
-                // reset sum to 0 if it's negative
-                sum = 0;
-            }
-        }
-        return maxi;
+  int maxSubArray(List<int> nums) {
+    int maxi = -1 << 31; // Equivalent to Integer.MIN_VALUE
+    int sum = 0;
+    
+    for (int num in nums) {
+      // calculating sub-array sum
+      sum += num;
+      maxi = sum > maxi ? sum : maxi;
+      
+      if (sum < 0) {
+        // reset sum to 0 if it's negative
+        sum = 0;
+      }
     }
+    
+    return maxi;
+  }
 }
